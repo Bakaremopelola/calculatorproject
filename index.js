c0fcd49b69000eb356595c64b823d34aaa7c9441
Bakaremopelola <152390768+Bakaremopelola@users.noreply.github.com>
2024-03-04 18:14:53 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+let m=document.getElementById("m")
+let c=document.getElementById("c")
+let seven=document.getElementById("seven")
+/* let cal3=document.getElementById("")
+let cal3=document.getElementById("")
+let cal3=document.getElementById("")
+let cal3=document.getElementById("")
+let cal3=document.getElementById("") */
+ let display=document.getElementById("display") 
+/* let cal3=document.getElementById("")
+let cal3=document.getElementById("")
+let cal3=document.getElementById("")
+let cal3=document.getElementById("")
+let cal3=document.getElementById("")
+let cal3=document.getElementById("")
+let cal3=document.getElementById("")
+let cal3=document.getElementById("")
+let cal3=document.getElementById("")
+let cal3=document.getElementById("")
+let cal3=document.getElementById("")
+let cal3=document.getElementById("") */
+
+
+seven.addEventListener("click",()=>{
+    display.value += "7";
+    
+});
